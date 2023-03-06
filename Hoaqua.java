9328935cba8f6bb6f62fef18d56b377e6693e94e
tminhky <125253696+tminhky@users.noreply.github.com>
2023-03-06 14:26:29 +0700

    Create Hoaqua.java

---
+++ Hoaqua.java
@@ -0,0 +1,40 @@
+package Lab5;
+
+public class Hoaqua {
+
+    private String ten;
+    private String kichthuoc;
+    private String mau;
+
+    public Hoaqua(String ten,String kichthuoc, String mau) {
+        this.ten = ten;
+        this.kichthuoc = kichthuoc;
+        this.mau = mau;
+    }
+
+    public String getTen() {
+        return ten;
+    }
+
+    public void setTen(String ten) {
+        this.ten = ten;
+    }
+
+    public String getKichthuoc() {
+        return kichthuoc;
+    }
+
+    public void setKichthuoc(String kichthuoc) {
+        this.kichthuoc = kichthuoc;
+    }
+
+    public String getMau() {
+        return mau;
+    }
+
+    public void setMau(String mau) {
+        this.mau = mau;
+    }
+
+}
+
