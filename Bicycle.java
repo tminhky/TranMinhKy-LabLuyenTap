0097982a4f9bf2772e4908eeda41c4c9a10b68d3
tminhky <125253696+tminhky@users.noreply.github.com>
2023-03-06 14:33:57 +0700

    Create Bicycle.java

---
+++ Bicycle.java
@@ -0,0 +1,37 @@
+package Cau2Lab5;
+
+public class Bicycle extends Vehicle {
+    private int giaban;
+    private String ngaynhap;
+    private String nguongoc;
+
+    public Bicycle(String color, String model, int giaban, String ngaynhap, String nguongoc)
+    {
+        super(color, model);
+        this.giaban = giaban;
+        this. ngaynhap = ngaynhap;
+        this.nguongoc = nguongoc;
+    }
+    public int getGiaban() {
+        return giaban;
+    }
+
+    public void setGiaban(int giaban) {
+        this.giaban = giaban;
+    }
+    public String getNgaynhap() {
+        return ngaynhap;
+    }
+
+    public void setNgaynhap(String ngaynhap) {
+        this.ngaynhap = ngaynhap;
+    }
+    public String getNguongoc() {
+        return nguongoc;
+    }
+
+    public void setNguongoc(String nguongoc) {
+        this.nguongoc = nguongoc;
+    }
+
+}
